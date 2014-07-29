aa50a9812e582d701ab2ad89f3db9ad2c4dd873d
Mohit Khanna <mohitk@icloud.com>
2014-07-29 23:42:39 +0530

    1. HTML5 Boilerplate 2. Restructuring

--- server.js
+++ server.js
@@ -4,28 +4,33 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require('compression');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
 // view engine setup
+app.enable('view cache');
 app.set('views', path.join(__dirname, 'assets/views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(__dirname + '/assets/public/images/favicon.ico'));
 app.use(logger('dev'));
+app.use(favicon(__dirname + '/assets/public/images/favicon.ico'));
+app.use(compression({ threshold: 0 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'assets/public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api', api);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
+	'use strict';
+
 	var err = new Error('Not Found');
 	err.status = 404;
 	next(err);
@@ -37,6 +42,8 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
 	app.use(function(err, req, res, next) {
+		'use strict';
+
 		res.status(err.status || 500);
 		res.render('error', {
 			message: err.message,
@@ -48,12 +55,13 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+	'use strict';
+
 	res.status(err.status || 500);
 	res.render('error', {
 		message: err.message,
 		error: {}
 	});
 });
-
 
 module.exports = app;
